434661695bd5d4be1d7c4ae17793712f8179f889
Jessie Gray <jessie.gray@ibm.com>
2022-06-15 20:22:04 -0400

    Declare program variables

---
+++ marioPyramid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Unlike for and while loops, which test the loop condition at the top of
+//the loop, the do...while loop in C programming checks its condition at
+//the bottom of the loop.
+
+int main(void)
+{
+    
+int height;
+int row;
+int column;
+
+    do
+    {
+        /* code */
+    } while (/* condition */);
+    
+}
